181808f81285c91df4385348f3be05eb0ccab0f4
adm <salemcat10@gmail.com>
2022-02-03 16:18:06 +0300

    03022022

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ship {
 
     private int[] ships = new int[4];
@@ -6,6 +8,8 @@ public class Ship {
         int num = 1;
 
         for (int i = 0; i < 4; i++){
+            Scanner console = new Scanner(System.in);
+            int x = console.nextInt();
             System.out.println("Создание ship " + num + " из 4, кол-во ячеек: ");
             ships[i] = size;
             num++;
